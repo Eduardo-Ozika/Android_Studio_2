dd3f636ed49d152f7fbfcc1c27a9f3628778aa87
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2024-10-29 20:21:47 -0300

    Criando projeto para acessar minha localizção

--- AlunosJsonComLibHttp/app/src/main/java/com/example/alunosjsoncomlibhttp/MainActivity.java
+++ AlunosJsonComLibHttp/app/src/main/java/com/example/alunosjsoncomlibhttp/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         textView = findViewById(R.id.dadosID);
         textIdade = findViewById(R.id.mediaIdade);
 
