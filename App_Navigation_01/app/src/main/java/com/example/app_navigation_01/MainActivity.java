e597c2b9b38fdb2e03880cec05e1f8f8b72491ba
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2024-11-12 16:21:24 -0300

    implementando logica dos fragmentos

--- App_Navigation_01/app/src/main/java/com/example/app_navigation_01/MainActivity.java
+++ App_Navigation_01/app/src/main/java/com/example/app_navigation_01/MainActivity.java
@@ -2,18 +2,61 @@ package com.example.app_navigation_01;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.example.app_navigation_01.fragment.CorinthiansFragment;
+import com.example.app_navigation_01.fragment.FlamengoFragment;
+import com.example.app_navigation_01.fragment.GoiasFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setItemIconTintList(null);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.nav_goias) {
+                selectedFragment = new GoiasFragment();
+            } else if (item.getItemId() == R.id.nav_flamengo) {
+                selectedFragment = new FlamengoFragment();
+            } else if (item.getItemId() == R.id.nav_corinthians) {
+                selectedFragment = new CorinthiansFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new FlamengoFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }
